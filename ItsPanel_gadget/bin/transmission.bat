7217e5092a8ddc7cafe0098109f9e7468c7a2e9a
74ls00 <userled@yandex.ru>
2018-08-25 21:09:18 +0300

    .

--- ItsPanel_gadget/bin/transmission.bat
+++ ItsPanel_gadget/bin/transmission.bat
@@ -1,3 +1,5 @@
-title Transmission running ...
-start /d G:\Programs\portableapps.com\PortableApps 
+@echo off
+title Transmission
+rem  running ...
+start /d G:\Programs\portableapps.com\PortableApps\TransmissionPortable TransmissionPortable.exe
 rem @start /d G:\Programs\ProgramsW\Transmission-Qt transmission_run.bat
