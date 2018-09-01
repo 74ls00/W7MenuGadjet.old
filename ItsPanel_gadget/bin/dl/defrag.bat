19ae0dfc281ddf3ca75eb176e0d870abd50bed51
74ls00 <userled@yandex.ru>
2018-09-01 19:52:25 +0300

    .

--- ItsPanel_gadget/bin/dl/defrag.bat
+++ ItsPanel_gadget/bin/dl/defrag.bat
@@ -10,17 +10,12 @@ set "programs_dir=%~d0%~p0..\..\programs\defrager"
 md "%programs_dir%"
 cd /d "%programs_dir%"
 cls
-
 if not exist="%programs_dir%\%defragger%" goto wgetadd
-
 goto runadd
-rem exit
-echo ---end---
-pause
+exit
 
 :wgetadd
 title Загрузка... %defname%
-
 echo.
 echo %defname% не найден и будет скачан скачан автоматически
 echo закройте окно если хотите отменить загрузку.
@@ -31,18 +26,15 @@ echo Будет установлен в
 echo "%programs_dir%ausdiskdefragportable.exe"
 echo.
 "%~d0%~p0..\exe\wget.exe" %defurl% --spider
-rem "%dl_dir%..\bin\wget.exe" %defurl% --spider
 title Загрузка... %defname%
 pause
-
 cls
 echo Загрузка... %defname%
 echo.
+echo Не закрывайте окно до завершения загрузки
+echo.
 "%~d0%~p0..\exe\wget.exe" %defurl% -t 5 -c -nc
-rem "%dl_dir%..\bin\wget.exe" %defurl% -t 5 -c -nc
 
 :runadd
 title Запуск... %defname%
-
 start /d "%programs_dir%" %defragger%
-rem exit
